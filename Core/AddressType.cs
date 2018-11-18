76a6b69473f6ecd00d31fc2efe883b4e1df73a63
ajay74 <andrew_james74@hotmail.com>
2018-11-18 14:25:04 +0100

    Add files via upload

---
+++ Core/AddressType.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Courby.Data;
+
+
+namespace Courby.Core
+{
+    public static class AddressType
+    {
+        /// <summary>
+        /// Adds an address type to the system.
+        /// </summary>
+        /// <param name="addressTypeName"></param>
+        /// <returns></returns>
+        public static int Add(string addressTypeName)
+        {
+            return Courby.Data.Connection.ExecuteProcedureNonQuery
+                ("InsertAddressType", new Connection.ParamData() { name = "@addressTypeName", value = addressTypeName, output = false });
+        }
+        /// <summary>
+        /// Removes a address type from the system.
+        /// </summary>
+        /// <param name="addressTypeName"></param>
+        /// <returns></returns>
+        public static int Delete(string addressTypeName)
+        {
+            return Courby.Data.Connection.ExecuteProcedureNonQuery
+                ("DeleteAddressType",
+                    new Connection.ParamData() { name = "@addressTypeId", value = DBNull.Value, output = false },
+                    new Connection.ParamData() { name = "@addressTypeId", value = addressTypeName, output = false });
+        }
+        /// <summary>
+        /// Removes a address type from the system.
+        /// </summary>
+        /// <param name="addressTypeId"></param>
+        /// <returns></returns>
+        public static int Delete(int addressTypeId)
+        {
+            return Courby.Data.Connection.ExecuteProcedureNonQuery
+                ("DeleteAddressType",
+                    new Connection.ParamData() { name = "@addressTypeId", value = addressTypeId, output = false },
+                    new Connection.ParamData() { name = "@addressTypeId", value = DBNull.Value, output = false });
+        }
+
+        /// <summary>
+        /// Gets the address types table stream.
+        /// </summary>
+        /// <returns></returns>
+        public static System.Data.SqlClient.SqlDataReader GetAddressTypes()
+        {
+            return Courby.Data.Connection.ExecuteProcedure("GetAddressTypes");
+        }
+        
+    }
+}
