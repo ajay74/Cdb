7dae55d1f8a55723c576f225a58a4ea6f280e70b
ajay74 <andrew_james74@hotmail.com>
2018-11-19 20:51:34 +0100

    Add files via upload

---
+++ ResourceData/Resources.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Data;
+
+namespace Courby.Resource.Data
+{
+    public static class Resources
+    {
+        public static string GetResourceValue(string culture, string ResourceKey)
+        {
+            return "";
+        }
+        public static void GetResourceValues(string culture, params string[] ResourceKeys)
+        {
+            Data.Connection.ExecuteProcedure("resources_GetResourceValues",
+                new Connection.ParamData() { name = "", value = "" },
+                new Connection.ParamData() { name= "", value=""}
+                );
+        }
+    }
+}
