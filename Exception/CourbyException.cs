651f8a0008c057d782be33ce732777da58e51a2d
ajay74 <andrew_james74@hotmail.com>
2018-11-19 20:49:19 +0100

    Add files via upload

---
+++ Exception/CourbyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Courby.Exception
+{
+    public class CourbyException : System.Exception
+    {
+        public CourbyException() : this("") { }
+        public CourbyException(System.Exception innerException) : this(innerException.Message, innerException) { }
+        public CourbyException(string message) : this (message, new System.Exception(message)) { }
+        public CourbyException(string message, System.Exception innerException) : base(message, innerException) { }
+
+    }
+}
