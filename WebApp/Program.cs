c3080b92a18f06d1d22904abab2b5a0f4bee497f
ajay74 <andrew_james74@hotmail.com>
2018-11-20 09:33:46 +0100

    Added web application

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TestApp3
+namespace Courby
 {
     public class Program
     {
